e60ef8ef7126872cc42233f6d5d119c345094b61
dhruv3694 <dhruv2005124@gmail.com>
2026-06-04 10:05:16 +0530

    23rd

--- practice5.js
+++ practice5.js
@@ -2,7 +2,37 @@
 let line = document.getElementById("a");
 console.log(line);
 
-function change(){
-    line.textContent="javascript is really tricky";
-    line.style.color="aqua";
+//function change(){
+//    line.textContent="javascript is really tricky";
+//    line.style.color="aqua";
+//}
+
+btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+
+    if(document.querySelector("h1").textContent=="now i know dom manipulation" ){
+        document.querySelector("h1").textContent="javascript is really tricky";
+        document.querySelector("h1").style.color="aqua";
+    }else{
+        document.querySelector("h1").textContent="now i know dom manipulation";
+        document.querySelector("h1").style.color="white";
+
+    }
+
+
+
+})
+
+
+//let btn=document.querySelector("button");
+//btn.addEventListener("click",function(){
+//    document.querySelector("h1").textContent="javascript is really tricky";
+//    document.querySelector("h1").style.color="aqua";
+//}) 
+
+age=22;
+if(age>=18){
+    console.log("you can vote");
+}else{
+    console.log("you cannot vote");
 }
